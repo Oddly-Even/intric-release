fd59f3e97380844af3fdf8699921c0bb1e8eed9d
Alexander Andersson <alexander@devil.se>
2025-05-27 15:19:57 +0200

    remove initial locale to try and fix flicker

--- frontend/apps/web/src/lib/i18n/index.ts
+++ frontend/apps/web/src/lib/i18n/index.ts
@@ -7,11 +7,8 @@ import { init, register } from 'svelte-i18n'
 register('en', () => import('./locales/en.json'))
 register('sv', () => import('./locales/sv.json'))
 
-const fallbackLocale = 'en'
-
-// Initialize i18n with default locale
+// Initialize i18n without a default locale
 // The actual locale will be set in the layout load function
 init({
-  fallbackLocale,
-  initialLocale: fallbackLocale,
+  fallbackLocale: 'en'
 })
